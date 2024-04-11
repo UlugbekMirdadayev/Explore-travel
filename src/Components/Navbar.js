94b53e35d3e61ada636dfe11a23d202916071e25
Мирдадаев Улугбек <81566035+UlugbekMirdadayev@users.noreply.github.com>
2024-04-11 11:35:24 +0000

    update 46 files

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,36 +1,37 @@
-import React, { useMemo } from 'react'
-import logo from "../assets/logo.png"
-import { useStoreState } from '../redux/selector'
-import locale from "../localization/locale.json"
+import React, { useMemo } from "react";
+import logo from "../assets/logo.png";
+import { useStoreState } from "../redux/selector";
+import locale from "../localization/locale.json";
 import { useDispatch } from "react-redux";
 import { setLang } from "../redux/lang";
-
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   const states = useStoreState();
   const langData = useMemo(() => locale[states.lang], [states.lang]);
   const dispatch = useDispatch();
 
-
   return (
-    <div className='Navbar'>
+    <div className="Navbar">
       <img src={logo} alt="" />
 
       <div className="cart">
-        <span>{langData.home}</span>
-        <span>{langData.destination}</span >
-        <span>{langData.about}</span >
-        <span>{langData.partner}</span>
-        <button className='btn'>{langData.login}</button>
-        <button className='btn'>{langData.register}</button>
-        <select name="" id="" onChange={(e) => dispatch(setLang(e.target.value))}>
+        <NavLink>{langData.home}</NavLink>
+        <NavLink>{langData.destination}</NavLink>
+        <NavLink>{langData.about}</NavLink>
+        <NavLink>{langData.partner}</NavLink>
+        <button className="btn">{langData.login}</button>
+        <button className="btn">{langData.register}</button>
+        <select
+          value={states.lang}
+          onChange={(e) => dispatch(setLang(e.target.value))}
+        >
           <option value="uz">UZ</option>
           <option value="en">EN</option>
         </select>
-
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/Components/Testim.js
+++ src/Components/Testim.js
@@ -1,36 +1,37 @@
-import React, { useMemo } from 'react'
-import { useStoreState } from '../redux/selector'
-import locale from "../localization/locale.json"
-import Frame from "../assets/Vector (2).png"
-import First from "../assets/photo.png"
+import React, { useMemo } from "react";
+import { useStoreState } from "../redux/selector";
+import locale from "../localization/locale.json";
+import Frame from "../assets/Vector (2).png";
+import First from "../assets/photo.png";
 
 function Testim() {
-    const states = useStoreState();
-    const langData = useMemo(() => locale[states.lang], [states.lang])
+  const states = useStoreState();
+  const langData = useMemo(() => locale[states.lang], [states.lang]);
 
-    return (
-        <div className='Testimonials'>
-
-            <div className="full-cart">
-            <h1>{langData.test}</h1>
-            <div className="icon-cart">
-            <div className="imgs">
-                <img src={Frame} alt="" />
-                <img src={Frame} alt="" />
-                <img src={Frame} alt="" />
-                <img src={Frame} alt="" />
-                <img src={Frame} alt="" />
-            </div>
-            <p>{langData.up}</p>
-            </div> 
-           <div className="cartt">
-            <h2>{langData.never}</h2>
-            <h5>{langData.forget}</h5>
-            </div>
-            </div>
-                <img src={First} alt="" />
+  return (
+    <div className="Testimonials">
+      <div className="full-cart">
+        <h1>{langData.test}</h1>
+        <div className="icon-cart">
+          <div className="imgs">
+            {Array(5)
+              .fill(0)
+              .map((_, i) => (
+                <img key={i} src={Frame} alt="star" />
+              ))}
+          </div>
+          <p>{langData.up}</p>
         </div>
-    )
+        <div className="cartt">
+          <h2>{langData.never}</h2>
+          <h5>{langData.forget}</h5>
+        </div>
+      </div>
+      <img src={First} alt="" />
+      <img src={First} alt="" />
+      <img src={First} alt="" />
+    </div>
+  );
 }
 
-export default Testim
+export default Testim;
